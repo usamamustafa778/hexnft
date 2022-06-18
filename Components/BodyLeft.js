b01d58b84930a736e9c7be31fb70c0eaf033f6ae
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2022-06-18 04:05:42 -0700

    push

--- Components/BodyLeft.js
+++ Components/BodyLeft.js
@@ -10,42 +10,42 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 
 export default function BodyLeft() {
   return (
-    <div className="hidden lg:flex flex-col items-start pt-12 pl-24 text-white h-screen sticky left-0 top-0">
+    <div className="hidden lg:flex flex-col items-start pt-12 pl-20 text-white h-screen sticky left-0 top-0">
       <Link href="/">
         <img src="logo.svg" className="h-8 mix-blend-normal cursor-pointer mb-4" alt="" />
       </Link>
       <Link href="/" className="flex items-center">
-        <p className="text-lg font-semibold mt-8 cursor-pointer">
+        <p className="text-lg font-semibold mt-6 cursor-pointer">
           <HomeIcon /> Home
         </p>
       </Link>
       <Link href="/about" className="flex items-center">
-        <p className="text-lg font-semibold mt-8 cursor-pointer">
+        <p className="text-lg font-semibold mt-6 cursor-pointer">
           <FavoriteIcon /> About
         </p>
       </Link>
       <Link href="/roadmap" className="flex items-center">
-        <p className="text-lg font-semibold mt-8 cursor-pointer">
+        <p className="text-lg font-semibold mt-6 cursor-pointer">
           <MapIcon /> Roadmap
         </p>
       </Link>
       <Link href="/mint" className="flex items-center">
-        <p className="text-lg font-semibold mt-8 cursor-pointer">
+        <p className="text-lg font-semibold mt-6 cursor-pointer">
           <AutoFixHighIcon /> Mint
         </p>
       </Link>
       <Link href="/smartcontract" className="flex items-center">
-        <p className="text-lg font-semibold mt-8 cursor-pointer">
+        <p className="text-lg font-semibold mt-6 cursor-pointer">
           <TipsAndUpdatesIcon /> Smart Contract
         </p>
       </Link>
       <Link href="/guestartist" className="flex items-center">
-        <p className="text-lg font-semibold mt-8 cursor-pointer">
+        <p className="text-lg font-semibold mt-6 cursor-pointer">
           <ColorLensIcon /> Guest Artist
         </p>
       </Link>
       <a href="https://twitter.com/hexinft" className="flex items-center">
-        <p className="text-lg font-semibold mt-8 cursor-pointer">
+        <p className="text-lg font-semibold mt-6 cursor-pointer">
           <TwitterIcon /> Twitter
         </p>
       </a>
