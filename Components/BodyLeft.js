25b062c95fa8baa8f0aaaf620c3416d0ef97a7cb
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2022-06-18 22:26:58 -0700

    push

--- Components/BodyLeft.js
+++ Components/BodyLeft.js
@@ -7,48 +7,63 @@ import AutoFixHighIcon from "@mui/icons-material/AutoFixHigh";
 import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
 import ColorLensIcon from "@mui/icons-material/ColorLens";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import MintButton from "./MintButton";
 
 export default function BodyLeft() {
   return (
     <div className="hidden lg:flex flex-col items-start pt-12 pl-20 text-white h-screen sticky left-0 top-0">
       <Link href="/">
-        <img src="logo.svg" className="h-8 mix-blend-normal cursor-pointer mb-4" alt="" />
+        <img src="logo.svg" className="h-7 mix-blend-normal cursor-pointer" alt="" />
       </Link>
       <Link href="/" className="flex items-center">
-        <p className="text-lg font-semibold mt-6 cursor-pointer">
+        <p className="text-lg font-semibold mt-5 cursor-pointer">
           <HomeIcon /> Home
         </p>
       </Link>
       <Link href="/about" className="flex items-center">
-        <p className="text-lg font-semibold mt-6 cursor-pointer">
+        <p className="text-lg font-semibold mt-5 cursor-pointer">
           <FavoriteIcon /> About
         </p>
       </Link>
       <Link href="/roadmap" className="flex items-center">
-        <p className="text-lg font-semibold mt-6 cursor-pointer">
+        <p className="text-lg font-semibold mt-5 cursor-pointer">
           <MapIcon /> Roadmap
         </p>
       </Link>
       <Link href="/mint" className="flex items-center">
-        <p className="text-lg font-semibold mt-6 cursor-pointer">
+        <p className="text-lg font-semibold mt-5 cursor-pointer">
           <AutoFixHighIcon /> Mint
         </p>
       </Link>
       <Link href="/smartcontract" className="flex items-center">
-        <p className="text-lg font-semibold mt-6 cursor-pointer">
+        <p className="text-lg font-semibold mt-5 cursor-pointer">
           <TipsAndUpdatesIcon /> Smart Contract
         </p>
       </Link>
       <Link href="/guestartist" className="flex items-center">
-        <p className="text-lg font-semibold mt-6 cursor-pointer">
+        <p className="text-lg font-semibold mt-5 cursor-pointer">
           <ColorLensIcon /> Guest Artist
         </p>
       </Link>
-      <a href="https://twitter.com/hexinft" className="flex items-center">
-        <p className="text-lg font-semibold mt-6 cursor-pointer">
+      <a href="https://twitter.com/hexinft" target="_blank" className="flex items-center">
+        <p className="text-lg font-semibold mt-5 cursor-pointer">
           <TwitterIcon /> Twitter
         </p>
       </a>
+      <a href="https://opensea.io/collection/hexi" target="_blank" className="flex items-center">
+        <p className="text-lg flex items-center font-semibold mt-5 cursor-pointer">
+           <img src="opensea.svg" className="h-5 mr-2" alt="" />
+           Opensea
+        </p>
+      </a>
+      <a href="https://open.spotify.com/playlist/0xca5KqJGLp1JucHao5sfu?si=4b11f728a19e4dd2" target="_blank" className="flex items-center">
+        <p className="text-lg flex items-center font-semibold mt-5 cursor-pointer">
+           <img src="spotify.svg" className="h-5 mr-2" alt="" />
+           Spotify
+        </p>
+      </a>
+      <br />
+      <MintButton/>
     </div>
   );
 }
--- Components/BodyMiddle.js
+++ Components/BodyMiddle.js
@@ -6,7 +6,7 @@ import FmdGoodOutlinedIcon from "@mui/icons-material/FmdGoodOutlined";
 import InsertLinkOutlinedIcon from "@mui/icons-material/InsertLinkOutlined";
 import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
 
-export default function BodyMiddle({ pfp }) {
+export default function BodyMiddle({ pfp, title }) {
   return (
     <div className="border-left border-l border-r border-white/10 h-full">
       <img src="banner.png" className="w-full h-46" alt="" />
@@ -20,7 +20,7 @@ export default function BodyMiddle({ pfp }) {
           <FollowButton link="https://twitter.com/hexinft" />
         </div>
 
-        <BodyMiddleTitle title="Hexi NFT" />
+        <BodyMiddleTitle title={title} />
         <MintButton />
         <div className="flex text-white/50 mt-3">
           <div className="flex items-center">
@@ -42,28 +42,63 @@ export default function BodyMiddle({ pfp }) {
           </div>
         </div>
       </div>
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
-      <Tweet />
+      <Tweet
+        text='perfectly shaped for your pfp ❤️ first 2.5k mints are free all the
+        rest are .0069'
+        time="29m"
+      />
+      <Tweet
+        text='we will have a total supply of 12345 NFTs in the 
+        HEXI collection! the first 2.5k are FREEEEEEE'
+        time="31m"
+      />
+      <Tweet
+        text='our ERC721A contract is SUPER optimized
+        that means gas is cheap, unlike the gas you put
+        in your car 🚗 🚘 🚙'
+        time="44m"
+      />
+      <Tweet
+        text="utility? it's literally made for your pfp. that's it.
+        come on - you aren't here for utility
+        you're a degen, and hexi is 🚀 🌙"
+        time="1h"
+      />
+      <Tweet
+        text="roadmap? upon mint completion, the entire hexi holder community will vote on the future of hexi: discord? alpha groups? incubator space for new projects? moon? sun? beyond?
+        we'll build together, because we are better together"
+        time="3h"
+      />
+      <Tweet
+        text="did you see our guest artists list 👉👉👉
+        each artist contributed a few 1/1s to our hexi
+        collection. as the cool kids say: LOOKS RARE."
+        time="3h"
+      />
+      <Tweet
+        text="there's nothing else down here
+        it's not your typical website with a footer
+        no footer, just hexi
+        wanna know who built the site though?
+        @jamesrichardfry did"
+        time="40d"
+      />
+      
     </div>
   );
 }
 
-function Tweet() {
+function Tweet({ text, time }) {
   return (
     <div className="flex items-center p-4 border-b border-white/10 text-white">
       <img src="home_pfp.svg" className="h-12 w-12" alt="" />
       <div className="ml-2">
         <div className="flex items-center">
           <h1 className="font-bold">Hexi NFT</h1>
-          <h1 className="text-sm text-white/50 ml-3">@HexiNFT - 29m</h1>
+          <h1 className="text-sm text-white/50 ml-3">@HexiNFT - {time}</h1>
         </div>
         <p>
-          perfectly shaped for your pfp ❤️ first 2.5k mints are free all the
-          rest are .0069
+          {text}
         </p>
       </div>
     </div>
